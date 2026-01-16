b6d700e2efe2178cc70c364f6c9de4c7da74ed7a
IIraycastII <raycast108@gmail.com>
2026-01-16 10:46:57 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (84.06%) - LeetSync

--- 258-add-digits/add-digits.py
+++ 258-add-digits/add-digits.py
@@ -1,14 +1,13 @@
 class Solution(object):
     def addDigits(self, num):
-        list_1 = []
-
-        if num != 0:
+        if len(str(num)) != 1:
             while len(str(num)) != 1:
-                for i in str(num):
-                    list_1.append(int(i))
-                num = sum(list_1)
-                list_1[:] = []
+                a = int(num / 10)
+                b = num - (int(num / 10) * 10)
+                num = a + b
 
+            return num
+        elif len(str(num)) == 1:
             return num
         else:
             return 0
