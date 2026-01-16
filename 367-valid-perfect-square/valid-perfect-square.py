710977a865e12326fa9d31eceea7f6881565bb4a
IIraycastII <raycast108@gmail.com>
2026-01-16 13:12:41 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (53.25%) - LeetSync

--- 367-valid-perfect-square/valid-perfect-square.py
+++ 367-valid-perfect-square/valid-perfect-square.py
@@ -1,14 +1,6 @@
 class Solution(object):
     def isPerfectSquare(self, num):
-        n = 0
-        a = 0
-        while True:
-            n = a * a
-            if n == num:
-                return True
-                break
-            elif n > num:
-                return False
-                break
-
-            a += 1
+        if (num ** 0.5) % 1 == 0:
+            return True
+        else:
+            return False
