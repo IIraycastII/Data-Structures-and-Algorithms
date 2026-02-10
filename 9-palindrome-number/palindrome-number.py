1a795f09f31fb76d4a6899036101c2dc91ad009d
IIraycastII <raycast108@gmail.com>
2026-02-10 11:17:46 +0530

    Time: 8 ms (71.47%) | Memory: 12.5 MB (18.32%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,6 +1,7 @@
 class Solution(object):
     def isPalindrome(self, x):
-        return str(x) == str(x)[::-1]
+        str_x = str(x)
+        return str_x == str_x[::-1]
 
      
                 
