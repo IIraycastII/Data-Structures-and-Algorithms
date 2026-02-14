ceb13c85311902a50a4df49237970bd2cf6e0866
IIraycastII <raycast108@gmail.com>
2026-02-14 10:21:32 +0530

    Time: 4 ms (46.79%) | Memory: 12.5 MB (45.43%) - LeetSync

--- 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.py
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.py
@@ -1,16 +1,8 @@
 class Solution(object):
     def firstPalindrome(self, words):
-        validate = 0
         for i in words:
             if i == i[::-1]:
-                validate = 1
                 return i
-                break
-            if i != i[::-1]:
-                validate = 0
-        
-        if validate == 0:
-            return ""
-
+        return ""
 
         
