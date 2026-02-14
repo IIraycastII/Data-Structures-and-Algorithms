72dfec1437a1eb9fd8ade4d1ea4c4c2e457f2e77
IIraycastII <raycast108@gmail.com>
2026-02-14 11:13:55 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (25.60%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.py
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.py
@@ -1,10 +1,3 @@
 class Solution(object):
     def checkIfPangram(self, sentence):
-        list_1 = []
-        for i in sentence:
-            list_1.append(i)
-        
-        if len(list(set(list_1))) == 26:
-            return True
-        else:
-            return False
+        return len(set(sentence)) == 26
