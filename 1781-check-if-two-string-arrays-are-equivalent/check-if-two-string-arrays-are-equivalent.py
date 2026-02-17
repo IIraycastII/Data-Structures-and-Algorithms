befd7215a8399d79fd1d88a7f392f1ef3be7efdc
IIraycastII <raycast108@gmail.com>
2026-02-17 21:08:59 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (65.31%) - LeetSync

---
+++ 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def arrayStringsAreEqual(self, word1, word2):
+        a = ""
+        b = ""
+
+        for i in word1:
+            a += i
+                
+        for i in word2:
+            b += i
+        
+        return a == b
