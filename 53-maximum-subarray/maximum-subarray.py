ce04d4f8fc56429675388cc7b733b5069e95a374
IIraycastII <raycast108@gmail.com>
2026-02-17 13:34:30 +0530

    Time: 51 ms (83.14%) | Memory: 22.1 MB (14.36%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        current_sum = 0
+        max_sum = nums[0]
+
+        for i in nums:
+            current_sum += i
+            max_sum = max(current_sum, max_sum)
+
+            if current_sum < 0:
+                current_sum = 0
+            
+        
+        return max_sum
+
