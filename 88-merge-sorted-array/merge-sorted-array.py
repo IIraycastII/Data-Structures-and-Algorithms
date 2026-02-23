39bd9f200e0c2857fd6ef0c3824e8f556cfd5eff
IIraycastII <raycast108@gmail.com>
2026-02-23 22:54:03 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (57.94%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -1,19 +1,14 @@
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
-        a = 0
-        b = 0
-
         list_1 = []
-        list_2 = []
 
-        while a != m:
-            list_1.append(nums1[a])
-            a += 1
-
-        while b != n:
-            list_2.append(nums2[b])
-            b += 1
-
-        list_1.extend(list_2)
-        list_1.sort()
+        for i in range(m):
+            list_1.append(nums1[i])
+        
+        for i in range(n):
+            list_1.append(nums2[i])
+        
         nums1[:] = list_1
+        nums1.sort()
+
+        return nums1
