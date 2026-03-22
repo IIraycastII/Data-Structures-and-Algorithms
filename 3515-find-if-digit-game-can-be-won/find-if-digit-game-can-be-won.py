6e5330fcc775abc7d783a510e90151048d68b0ca
IIraycastII <raycast108@gmail.com>
2026-03-22 20:19:03 +0530

    Time: 4 ms (21.09%) | Memory: 12.3 MB (78.46%) - LeetSync

--- 3515-find-if-digit-game-can-be-won/find-if-digit-game-can-be-won.py
+++ 3515-find-if-digit-game-can-be-won/find-if-digit-game-can-be-won.py
@@ -4,12 +4,11 @@ class Solution(object):
         double_digit = []
 
         for i in nums:
-            if len(str(i)) == 1:
+            if i <= 9:
                 single_digit.append(i)
-            elif len(str(i)) == 2:
+            else:
                 double_digit.append(i)
         
         if sum(single_digit) > sum(double_digit) or sum(single_digit) < sum(double_digit):
             return True
-        else:
-            return False
+        return False
